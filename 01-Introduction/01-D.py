11578814f55f3d0e88dae23bb6a40d4fd06f0b3a
ItsNiklas <nikbau2000@gmail.com>
2022-10-29 08:18:12 +0000

    Use generator expression for reduced memory

--- 01-Introduction/01-D.py
+++ 01-Introduction/01-D.py
@@ -4,7 +4,7 @@ def main() -> None:
     
     # Get only queue lines, by starting at index 2 and incrementing by 2.
     # Reverse each queue via slicing.
-    rev = [q[::-1] for q in input[2::2]]
+    rev = (q[::-1] for q in input[2::2])
     
     # Pretty print.
     print(*[' '.join(x) for x in rev], sep = '\n')
--- 01-Introduction/01-E.py
+++ 01-Introduction/01-E.py
@@ -1,15 +1,11 @@
-def squabbits(s: int) -> int:
-    # Described behaviour is quivalent to quadratic growth:
-    return (s+1)**2
-
-
 def main() -> None:
     # Read from stdin.
     input = [int(x) for x in open(0)]
                 
     # Execute function on every line after the first.
+    # Described behaviour is quivalent to quadratic growth.
     # Pretty print.
-    print(*[squabbits(x) for x in input[1:]], sep = '\n')
+    print(*[(x+1)**2 for x in input[1:]], sep = '\n')
 
 
 if __name__ == '__main__':
