ec680c72546c77972eed710452be383051fa2841
ItsNiklas <nikbau2000@gmail.com>
2022-11-11 21:49:30 +0000

    Removed unnecessary array 03-C

--- 03-DP/C-03.py
+++ 03-DP/C-03.py
@@ -1,25 +1,19 @@
-import copy
-
-
 def main() -> None:
     # Read from stdin.
-    input = [list(map(int, x.split())) for x in open(0)][1:]
+    mountain = [list(map(int, x.split())) for x in open(0)][1:]
 
-    # Deepcopy to create our cache.
-    output = copy.deepcopy(input)
-
-    for level_id in range(1, len(input)):
+    for level_id in range(1, len(mountain)):
         for slope_id in range(level_id + 1):
             # Iterate over all slopes.
             # For each slope, determine max of parents, if they exist.
-            a = output[level_id - 1][slope_id - 1] if 0 <= slope_id - 1 < level_id else float('-inf')
-            b = output[level_id - 1][slope_id] if 0 <= slope_id < level_id else float('-inf')
+            a = mountain[level_id - 1][slope_id - 1] if 0 <= slope_id - 1 < level_id else float('-inf')
+            b = mountain[level_id - 1][slope_id] if 0 <= slope_id < level_id else float('-inf')
 
             # Write into cache.
-            output[level_id][slope_id] += max(a, b)
+            mountain[level_id][slope_id] += max(a, b)
 
     # Get maximum score at the the lowest level.
-    print(max(output[-1]))
+    print(max(mountain[-1]))
 
     
 if __name__ == '__main__':
