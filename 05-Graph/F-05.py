043fb1d8c84653e73bfa1b7917ddfb555cbb8c6e
ItsNiklas <nikbau2000@gmail.com>
2022-11-27 11:56:15 +0000

    Finally F-05.py

--- 05-Graph/F-05.py
+++ 05-Graph/F-05.py
@@ -3,106 +3,72 @@ from itertools import product
 
 
 def main() -> None:
-    def print_g(g, p = None):
-        if p is not None:
-            for i in range(N):
-                for j in range(M):
-                    c = p.index((i, j)) if (i, j) in p else g[i][j]
-                    print(c, end=" ")
-                print()
-        elif type(g) == dict:
-            for i in range(N):
-                for j in range(M):
-                    c = g.get((i,j), -1)
-                    print(c if c != -1 else grid[i][j], end=" ")
-                print()
-        else:
-            for i in range(len(g)): print(*g[i], sep=" ")
-
     # Read from stdin.
     input = [list(x.rstrip()) for x in open(0)]
     grid = input[1:]
     N, M = len(grid), len(grid[0])
 
-    # Create Adjacency list.
+    # Create neighbors function.
     def neighbors(loc):
         x, y = loc
-        nb = [(x, y+1), (x+1, y), (x-1, y), (x, y-1)]
+        # Adjacent locations.
+        nb = [(x+1, y), (x-1, y), (x, y-1), (x, y+1)]
+        # Filter out of bounds and walls.
         nb = filter(lambda xy : 0 <= xy[0] < N and 0 <= xy[1] < M, nb)
         nb = filter(lambda xy : grid[xy[0]][xy[1]] != "#", nb)
-        return set(nb)
+        return nb
 
-    graph = {location : neighbors(location) for location in product(range(N), range(M))}
-
+    # Cost function
     def cost(from_from_node, from_node, to_node):
-        correction = 0
+        c = 0
+        
         if grid[next[0]][next[1]] == "D":
-            # Correct for starting position: right.
+            # Correct for starting direction: Right.
             dx, dy = to_node[0] - from_node[0], to_node[1] - from_node[1]
-            if dx == 0 and dy == 1:
-                correction += 2
-            elif dx == 0 and dy == -1:
-                correction += 0
-            else:
-                correction += 1
-        if from_from_node is None: return 1 + correction # First move
-        if to_node == from_from_node: return 3 + correction # Turn around
-        return 1 + (1 if to_node[0] != from_from_node[0] and to_node[1] != from_from_node[1] else 0) + correction
+            if dx != 0: c += 1
+            elif dy == 1: c += 2
+                
+        if from_from_node is None:
+            return 1 + c # First move
+        if to_node == from_from_node:
+            return 3 + c # Turn around
+            
+        # Are we turning? Add one more to the cost.
+        ddx, ddy = to_node[0] - from_from_node[0], to_node[1] - from_from_node[1]
+        return (2 if ddx != 0 and ddy != 0 else 1) + c
 
-    # ?
-    start = [x for x in grid if "C" in x]
-    if not any(start):
-        print("IMPOSSIBLE")
-        return
-    start = (grid.index(start[0]), start[0].index("C"))
+    # Find start (C)
+    start = [x for x in grid if "C" in x][0]
+    start = (grid.index(start), start.index("C"))
 
-    distance = {start: 0}
-
-    # BFS.
-
-    # Initialize came_from set and queue.
-    visited, q = {x : set() for x in graph.keys()}, deque()
-    visited[start].add(None)
-    q.append(start)
-
-    goal = None
+    # BFS/Floodfill
+    # Missing: Early stopping.
+    # Initialize came_from/distance dict and queue.
+    q = deque([start])
+    distance = {start : {(None, 0)}}
     score = float("inf")
 
     while q:
-        if goal is not None and max(distance.values()) > 3 * score:
-            break
         vertex = q.popleft()
 
-        for next in graph[vertex]:
-            # We found a faster way to get to next?
-            for parent in visited[vertex]:
-                new_dist = distance[vertex] + cost(parent, vertex, next)
-                if next in distance and new_dist == distance[next]:
-                    # New entry direction
-                    visited[next].add(vertex)
-                if next not in distance or new_dist < distance[next]:
-                    distance[next] = new_dist
-                    visited[next].add(vertex)
+        for next in neighbors(vertex):
+            # We need per-parent distances because of turning cost.
+            for parent, old_dist in distance[vertex]:
+                new_dist = old_dist + cost(parent, vertex, next)
 
-                    # Some goal found
-                    if grid[next[0]][next[1]] == "D" and new_dist < score:
-                        score, goal = new_dist, next
-
-                    # Missing: Early stopping
-                    #if goal is None:
+                # Add new connection.
+                if next in distance:
+                    distance[next].add((vertex, new_dist))
+                else:
+                    distance.update([(next, {(vertex, new_dist)})])
                     q.append(next)
-    if goal is None: score, goal = -1, (-1, -1)
 
-    ###
+                # Some goal found, new shortest path?
+                if grid[next[0]][next[1]] == "D":
+                    score = min(score, new_dist)
 
-    print_g(distance)
-
-    # Goal not reached
-    if sum(goal) < 0:
-        print("IMPOSSIBLE")
-        return
-
-    print(distance[goal])
+    # Output shortest path length if found.
+    print(score) if score != float("inf") else print("IMPOSSIBLE")
 
 
 if __name__ == '__main__':
